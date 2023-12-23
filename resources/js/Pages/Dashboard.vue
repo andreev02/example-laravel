49ab29bbfffdf7c5201fb1ceed1ffbe30c69b372
Alexei Andreev <leshakod@gmail.com>
2023-12-23 23:19:14 +0300

    Changed GitHub button href

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -17,7 +17,7 @@ import { Head, Link } from '@inertiajs/vue3';
                     <div class="pl-6 pt-6 text-gray-900 dark:text-gray-100">This is an example app for Perfect Panel, Sevastopol :)</div>
 
                     <div class="ml-5 pt-6 pd-6 px-6 sm:px-0 max-w-sm">
-                        <a href="https://github.com/andreev02">
+                        <a href="https://github.com/andreev02/example-laravel">
                             <button type="button"
                                 class="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center justify-between dark:focus:ring-[#4285F4]/55 mr-2 mb-2">
                                 <svg class="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
